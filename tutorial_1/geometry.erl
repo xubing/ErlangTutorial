030aa0f4e648b9000b55e7913cd04402d21bc4b4
zhangxubing <zhxubing@gmail.com>
2013-12-20 18:47:13 +0800

    add a code to show the variable

--- tutorial_1/geometry.erl
+++ tutorial_1/geometry.erl
@@ -1,11 +1,3 @@
--module(geometry).
--export([area/1]).
-
-%%%tip:
-%%%1  变量需要大写开头。 capitical Header.
-%%%  分割用;
-%%%  调用 erl test ， 
-%%%  c(geometry). 
-%%%  geometry:area({rectangle,10,5}).
-area({rectangle,Width,Height})->Width*Height;
-area({circle,R})->3.14159*R*R.
+%%%
+%%%
+%%%
