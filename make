fb7376bd53379725508fbb578da837f9ff40aeb6
zhangxubing <zhxubing@gmail.com>
2014-02-11 17:36:29 +0800

    add a test erl for math

---
+++ make
@@ -0,0 +1,18 @@
+.SUFFIXES: .erl .beam
+
+#OUT = ./beam
+
+.erl.beam:
+    #erlc -W $< ; mv $@ ${OUT}
+    erlc -W $< 
+    ERL = erl -boot start_clean
+    MODS = math
+
+all: compile
+compile: ${MODS:%=%.beam}
+@echo "make clean -clean up"
+# application:
+#     ${ERL} -s application start ARG1 ARG2
+
+clean:
+    rm -rf *.beam erl_crash.dump
---
+++ math.erl
@@ -0,0 +1,12 @@
+-module(math).
+-export([odds_and_evens/1]).
+
+odds_and_evens(L)->
+    odds_and_evens_acc(L,[],[]).
+odds_and_evens_acc([H|T],Odds,Evens)->
+    case (H rem 2) of
+        1->odds_and_evens_acc(T,[H|Odds],Evens);
+        0->odds_and_evens_acc(T,Odds,[H|Evens])
+    end;
+odds_and_evens_acc([],Odds,Evens)->
+    {Odds,Evens}.
